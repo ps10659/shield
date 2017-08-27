31aed1df12bd50c0e86b54a9fa6741c93a317bb1
Jacky Chen <jacky.c@17.media>
2017-08-27 23:15:59 +0800

    OK status, staring webhook

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"time"
+	// "strconv"
 	// "reflect"
 	// "fmt"
 	// "encoding/json"
@@ -21,23 +23,114 @@ type announcement struct {
 	Pic         string `yaml:"pic" json:"pic"`
 }
 
+type status struct {
+	Shielding_all bool                    `json:"shielding_all"`
+	Announcements map[string]announcement `json:"announcements"`
+}
+
 var (
+	// announcements is a global variable holding the information sourced from announcementURL
+	announcements          = map[string]announcement{}
 	announcementURL string = "https://raw.githubusercontent.com/ps10659/shield/master/announcement.yaml"
 
-	announcements = map[string]announcement{}
+	// shielding_all determines whether to send announcements response to the clients
+	// can also use shielding_iOS, shielding_android, shielding_web to block specific platform users
+	shielding_all = false
 
+	// update announcement from github at most once an updateDuration time
+	updateDuration  = time.Second * 5
+	lastUpadateTime time.Time
+
+	// err
 	ErrNoAnnouncement = errors.New("Announcement file not found")
 )
 
-func getAnnouncement(c *gin.Context, announcementURL string) error {
+func main() {
+	r := gin.Default()
+
+	// authorized user only
+	r.GET("/shield/set/:action", setShield)
+	r.GET("/shield/info", getShieldInfo)
+	r.GET("/webhook", webhook)
+
+	// for all users
+	r.GET("/announcement", getAnnouncement)
+
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
+
+func setShield(c *gin.Context) {
+	action := c.Param("action")
+
+	switch action {
+	case "on":
+		shielding_all = true
+		c.JSON(http.StatusOK, gin.H{"shielding_all": shielding_all})
+	case "off":
+		shielding_all = false
+		c.JSON(http.StatusOK, gin.H{"shielding_all": shielding_all})
+	default:
+		// Should this case return 4XX ???
+		c.Status(http.StatusNoContent)
+	}
+}
+
+func getShieldInfo(c *gin.Context) {
+	err := updateAnnouncement(c, announcementURL)
+	if err != nil {
+		// err
+	}
+
+	info := status{Shielding_all: shielding_all, Announcements: announcements}
+	c.JSON(http.StatusOK, info)
+}
+
+func webhook(c *gin.Context) {
+	err := updateAnnouncement(c, announcementURL)
+	if err != nil && err == ErrNoAnnouncement {
+		// c.Status(http.StatusNoContent)
+		c.Status(http.StatusNoContent)
+		return
+	}
+	return
+}
+
+func getAnnouncement(c *gin.Context) {
+	if shielding_all == false {
+		c.Status(http.StatusNoContent)
+		return
+	}
+
+	language := c.Request.Header.Get("language") // MustGet fail why???
+	duration := time.Now().Sub(lastUpadateTime)  // Will this line cause server bad performance?
+	if duration > updateDuration {
+		err := updateAnnouncement(c, announcementURL)
+		if err != nil && err == ErrNoAnnouncement {
+			// c.Status(http.StatusNoContent)
+			c.Status(http.StatusNoContent)
+			return
+		}
+		// if other err, do something
+	}
+
+	switch language {
+	case "TW":
+		c.JSON(http.StatusOK, announcements["TW"])
+	default:
+		c.JSON(http.StatusOK, announcements["EN"])
+	}
+	return
+}
+
+func updateAnnouncement(c *gin.Context, announcementURL string) error { //Still need to pass gin.Context?
+	lastUpadateTime = time.Now()
 	resp, err := http.Get(announcementURL)
 	if resp != nil {
 		defer resp.Body.Close()
 	}
-
-	if err != nil {
-		// handle err
-		//return nil, ErrNoAnnouncement
+	if err != nil { // & err == ErrFileNotFound
+		return ErrNoAnnouncement
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
@@ -52,57 +145,4 @@ func getAnnouncement(c *gin.Context, announcementURL string) error {
 	}
 
 	return nil
-}
-
-func main() {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
-
-	r.GET("/shield", func(c *gin.Context) {
-		// to be modified
-		language := c.Request.Header.Get("language")
-
-		err := getAnnouncement(c, announcementURL)
-		if err != nil {
-			if err == ErrNoAnnouncement {
-				// return http status 204
-			}
-			// other err do something?
-
-			return
-		}
-
-		switch language {
-		case "TW":
-			c.JSON(http.StatusOK, announcements["TW"])
-		default:
-			c.JSON(http.StatusOK, announcements["EN"])
-		}
-
-		return
-	})
-
-	r.GET("/shield0", func(c *gin.Context) {
-		err := getAnnouncement(c, announcementURL)
-		if err != nil {
-			if err == ErrNoAnnouncement {
-				// return http status 204
-			}
-			// other err do something?
-
-			return
-		}
-		c.JSON(http.StatusOK, announcements["TW"])
-
-		return
-	})
-
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
 }
